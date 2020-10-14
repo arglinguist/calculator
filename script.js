cfe1482dd361952f441900f1a95cd8592465f2c1
arglinguist <arglinguist@gmail.com>
2020-10-14 15:06:23 -0700

    outline in comments

---
+++ script.js
@@ -0,0 +1,22 @@
+/* 
+-make functions for each operation
+    - ADD
+    - SUBTRACT
+    - MULTIPLY
+    - DIVIDE (give snarky message for /0)
+-make OPERATE function that takes an operator & 2 numbers and calls one of the above
+- CLEAR function wipes ALL exisiting data
+- create function to populate DISPLAY & updates with solution when operate is called
+- *KEY* Figure out how to store all values and call operator function with them.
+
+WATCH OUT
+- evaluate only 2 at a time, even if a string of numbers & operators is inputted
+- round numbers with long decimals
+- what happens if pressing (=) before numbers or operator entered?
+
+BONUS
+- decimal (only once)
+- backspace
+- keyboard support(event listener)
+
+*/

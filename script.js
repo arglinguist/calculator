d26a23ca7fbead39d67f332cca6fe878e78f0d74
arglinguist <arglinguist@gmail.com>
2020-11-03 10:37:41 -0800

    A/C clears display

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ BONUS
 - keyboard support(event listener)
 */
 
-// Assign buttons for...
-// operators and digits
+// Assign buttons 
  const addButton = document.getElementById("add"),
      subButton = document.getElementById("sub"),
      mulButton = document.getElementById("mult"),
@@ -30,19 +29,95 @@ BONUS
      button7 = document.getElementById("7"),
      button8 = document.getElementById("8"),
      button9 = document.getElementById("9"),
-     button0 = document.getElementById("0");
-//special buttons
-const decButton = document.getElementById("."),
+     button0 = document.getElementById("0"),
+     decButton = document.getElementById("."),
      backButton = document.getElementById("back"),
      clearButton = document.getElementById("clear"),
      reverseButton = document.getElementById("rev"),
      percButton = document.getElementById("%");
 
-let display = document.getElementById("display");
+const display = document.getElementById("display");
+let currentDisplay = "0",
+    currentA = 0, 
+    currentB = 0,
+    currentResult = 0,
+    currentOperator = "+";
 
-function setDisplay(newContent){
-    display.textContent=newContent;
+//digits --- can this be consolidated into an array??
+button0.onclick = function(){
+    currentDisplay += "0";
+    setDisplay(currentDisplay);
+};
+button1.onclick = function(){
+    currentDisplay += "1";
+    setDisplay(currentDisplay);
+};
+button2.onclick = function(){
+    currentDisplay += "2";
+    setDisplay(currentDisplay);
+};
+button3.onclick = function(){
+    currentDisplay += "3";
+    setDisplay(currentDisplay);
+};
+button4.onclick = function(){
+    currentDisplay += "4";
+    setDisplay(currentDisplay);
+};
+button5.onclick = function(){
+    currentDisplay += "5";
+    setDisplay(currentDisplay);
+};
+button6.onclick = function(){
+    currentDisplay += "6";
+    setDisplay(currentDisplay);
+};
+button7.onclick = function(){
+    currentDisplay += "7";
+    setDisplay(currentDisplay);
+};
+button8.onclick = function(){
+    currentDisplay += "8";
+    setDisplay(currentDisplay);
+};
+button9.onclick = function(){
+    currentDisplay += "9";
+    setDisplay(currentDisplay);
+};
+clearButton.onclick = function(){
+    reset();
 }
+
+//functions
+function setDisplay(newContent){
+        //need to shorten if too long for display
+        //get rid of leading 0
+    display.textContent = newContent;
+}
+
+function clearDisplay(){
+    display.textContent = "0";
+}
+
+function reset(){
+    clearDisplay();
+    //do
+    currentA = 0, 
+    currentB = 0,
+    currentResult = 0,
+    currentOperator = "+";
+}
+
+
+function removeLast(str){
+    let length = str.length;
+        newStr = "";
+    for (i = 0; i<length-1; i++){
+        newStr += str.char[i];
+    }
+    return newStr;
+}
+
 
 // The maths...
 function operate (a, b, opp){
